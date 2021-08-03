7b1052b137443c5795f0969656bbc059f3b43fae
NikX <41104633+NikXpro@users.noreply.github.com>
2021-08-03 20:32:38 +0200

    add anim utils

--- client/utils/_function.lua
+++ client/utils/_function.lua
@@ -1,1 +1,29 @@
 LEEDS = {}
+
+function LEEDS.RequestAnimSet(animSet, cb)
+	if not HasAnimSetLoaded(animSet) then
+		RequestAnimSet(animSet)
+
+		while not HasAnimSetLoaded(animSet) do
+			Citizen.Wait(4)
+		end
+	end
+
+	if cb ~= nil then
+		cb()
+	end
+end
+
+function LEEDS.RequestAnimDict(animDict, cb)
+	if not HasAnimDictLoaded(animDict) then
+		RequestAnimDict(animDict)
+
+		while not HasAnimDictLoaded(animDict) do
+			Citizen.Wait(4)
+		end
+	end
+
+	if cb ~= nil then
+		cb()
+	end
+end
---
+++ client/utils/anim.lua
@@ -0,0 +1,33 @@
+LEEDS.Play = {};
+local Play = LEEDS.Play;
+
+function Play.Attitude(ped, lib, anim)
+	LEEDS.RequestAnimSet(lib, function()
+		SetPedMovementClipset(ped, anim, true)
+	end)
+end
+
+function Play.Anim(ped, lib, anim, flag)
+	LEEDS.RequestAnimDict(lib, function()
+		TaskPlayAnim(ped, lib, anim, 8.0, 8.0, -1, tonumber(flag), 0.0, false, false, false)
+	end)
+end
+
+function Play.Scenario(ped, anim)
+	TaskStartScenarioInPlace(ped, anim, 0, false)
+end
+
+function Play.RandomAnim(playerId, animTable)
+    local randomAnim = math.random(0, #animTable)
+    for k,v in pairs(animTable) do
+        if k == randomAnim then
+            if v.type == "anim" then
+                LEEDS.Play.Anim(playerId, v.lib, v.anim, v.flag)
+            elseif v.type == "attitude" then
+                LEEDS.Play.Attitude(playerId, v.lib, v.anim)
+            elseif v.type == "scenario" then
+                LEEDS.Play.Scenario(playerId, V.anim)
+            end
+        end
+    end
+end
