707a9a86996e75fc924355a71d4ff3fcec63551f
NikX <briffanathan781@gmail.com>
2021-12-24 03:29:36 +0100

    update data Selection

--- server/lifesystem/serverManager/sv_serverManager.lua
+++ server/lifesystem/serverManager/sv_serverManager.lua
@@ -1,9 +1,44 @@
-LEEDS.RegisterCallback("leeds:requestData", function(type, source, cb)
-    if type == "departement" then
-        local departementList = MySQL.Sync.execute("SELECT departement.id, departement.type, departement.name, departement.texture, positions.x, positions.y, positions.z, blips.blipName, blips.blipId, blips.blipType, blips.blipScale, blips.blipColor FROM `departement`, `positions`, `blips` WHERE positions.type = 'departement' AND departement.id = positions.of AND departement.id = blips.of AND blips.type = 'departement' AND departement.deleted = 0 LIMIT 0, 300")
-        cb(departementList)
+
+function updateData(source)
+    local Departement = {}
+    local loadDepartement = MySQL.Sync.execute("SELECT departement.id, departement.type, departement.name, departement.texture, positions.x, positions.y, positions.z FROM `departement`, `positions` WHERE positions.type = 'departement' AND departement.id = positions.of AND departement.deleted = 0 LIMIT 0, 300")
+    for i = 1, #loadDepartement do
+        Departement[i] = loadDepartement[i]
+        local loadGarage = MySQL.Sync.execute("SELECT garages.id, garages.type, garages.garageName, positions.x, positions.y, positions.z, positions.heading FROM `garages`,`positions` WHERE garages.of = @garage_of AND garages.type = 'departement' AND positions.type = 'garage' AND garages.id = positions.of", {["garage_of"] = Departement[i].id})
+        if loadGarage[1] then
+            Departement[i].garage = loadGarage
+            for j = 1, #Departement[i].garage do
+                local loadSpawnGarage = MySQL.Sync.execute("SELECT positions.x, positions.y, positions.z, positions.heading FROM `positions` WHERE positions.type = 'spawnGarage' AND positions.of = @of", {["of"] = Departement[i].garage[j].id})
+                Departement[i].garage[j].spawnList = loadSpawnGarage 
+            end
+        else
+            Departement[i].garage = {}
+        end
     end
+    TriggerClientEvent("leeds:updateData", source, Departement)
+end
+
+LEEDS.RegisterCallback("leeds:requestData", function(t, source, cb)
+    local Departement = {}
+    local loadDepartement = MySQL.Sync.execute("SELECT departement.id, departement.type, departement.name, departement.texture, positions.x, positions.y, positions.z FROM `departement`, `positions` WHERE positions.type = 'departement' AND departement.id = positions.of AND departement.deleted = 0 LIMIT 0, 300")
+    for i = 1, #loadDepartement do
+        Departement[i] = loadDepartement[i]
+        local loadGarage = MySQL.Sync.execute("SELECT garages.id, garages.type, garages.garageName, positions.x, positions.y, positions.z, positions.heading FROM `garages`,`positions` WHERE garages.of = @garage_of AND garages.type = 'departement' AND positions.type = 'garage' AND garages.id = positions.of", {["garage_of"] = Departement[i].id})
+        print(json.encode(loadGarage))
+        if loadGarage[1] then
+            Departement[i].garage = loadGarage
+            for j = 1, #Departement[i].garage do
+                local loadSpawnGarage = MySQL.Sync.execute("SELECT positions.x, positions.y, positions.z, positions.heading FROM `positions` WHERE positions.type = 'spawnGarage' AND positions.of = @of", {["of"] = Departement[i].garage[j].id})
+                Departement[i].garage[j].spawnList = loadSpawnGarage 
+            end
+        else
+            Departement[i].garage = {}
+        end
+        
+    end
+    cb(Departement)
 end)
+
 
 RegisterNetEvent("leeds:manageDepartement")
 AddEventHandler("leeds:manageDepartement", function(type, data)
@@ -35,14 +70,15 @@ AddEventHandler("leeds:manageDepartement", function(type, data)
                     ["blipScale"] = data.depBlipScale,
                     ["blipColor"] = data.depBlipColor,
                 })
+                updateData(src_)
             end)
         elseif type == "createGarage" then
-            MySQL.Async.execute("INSERT INTO `garage` (of, type, garageName) VALUES (@of, @type, @garageName)", {
+            MySQL.Async.execute("INSERT INTO `garages` (of, type, garageName) VALUES (@of, @type, @garageName)", {
                 ["of"] = data.depSelected,
                 ["type"] = "departement",
                 ["garageName"] = data.depGarageName
             }, function()
-                local id = MySQL.Sync.execute("SELECT id FROM `garage` WHERE type = 'departement' ORDER BY id DESC LIMIT 1", {})
+                local id = MySQL.Sync.execute("SELECT id FROM `garages` WHERE type = 'departement' ORDER BY id DESC LIMIT 1", {})
                 MySQL.Sync.execute("INSERT INTO positions (of, type, x, y, z) VALUES (@of, @type, @x, @y, @z)", {
                     ["of"] = id[1].id,
                     ["type"] = "garage",
@@ -51,6 +87,7 @@ AddEventHandler("leeds:manageDepartement", function(type, data)
                     ["z"] = data.depGarageCoords[3],
                     ["heading"] = data.depGarageHeading
                 })
+                updateData(src_)
             end)
         elseif type == "edit" then
             
@@ -58,6 +95,7 @@ AddEventHandler("leeds:manageDepartement", function(type, data)
         elseif type == "delete" then
             MySQL.Sync.execute("UPDATE `departement` SET deleted = 1 WHERE id = @id", {["id"] = data})
             TriggerEvent("LEEDS-CallBackServer:leeds:requestData", "departement")
+            updateData(src_)
         end
 
     else
